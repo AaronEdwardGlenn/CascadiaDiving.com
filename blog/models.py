b3329bd6e204ed922beb9d5b549e27e25468166a
Aaron Edward Glenn <53454869+AaronEdwardGlenn@users.noreply.github.com>
2020-06-11 15:03:18 -0700

    added Post model to models.py and added a defualt 'deleted' model to set deleted Users with onetomany relationship with Posts

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+def get_sentinel_user():
+    return get_user_model().objects.get_or_create(username='deleted')[0]
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.SET(get_sentinel_user))
