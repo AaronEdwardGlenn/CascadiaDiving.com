0a1584a9975a2921ac1d05d165012221ca68b40b
Aaron Edward Glenn <53454869+AaronEdwardGlenn@users.noreply.github.com>
2020-06-12 16:50:26 -0700

    added custom images for carousel

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='blog-home'),
     path('about/', views.about, name='blog-about'),
+    path('carousel/', views.carousel, name='blog-carousel'),
+
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,26 +1,10 @@
 from django.shortcuts import render
 from .models import DivePost
-dives = [
-    {
-        'site': 'Leafy Greans',
-        'divers': 'Aaron Glenn',
-        'date_dove': 'May 20, 2020',
-        'content': 'awesome training site'
-    },
-    {
-        'site': 'Dougan Falls',
-        'divers': 'Aaron Glenn',
-        'date_dove': 'June 2, 2020',
-        'content': 'crazy waterfall'
-    }
-]
 
 
 def home(request):
     context = {
-        'posts': DivePost.objects.all(),
-        'title': 'home'
-
+        'posts': DivePost.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
@@ -28,4 +12,6 @@ def home(request):
 def about(request):
     return render(request, 'blog/about.html')
 
-# Create your views here.
+
+def carousel(request):
+    return render(request, 'blog/carousel.html')
