b82463fc11c328a465e04eab3a11ab49d624208a
Aaron Edward Glenn <53454869+AaronEdwardGlenn@users.noreply.github.com>
2020-06-17 17:16:32 -0700

    added join view

--- join/views.py
+++ join/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def join(request):
+    context = {
+        'join': 'active'
+    }
+    return render(request, 'join/join.html', context)
