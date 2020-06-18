8658d4a80cca04080cb515b1b42600c2245b727c
Aaron Edward Glenn <53454869+AaronEdwardGlenn@users.noreply.github.com>
2020-06-17 17:17:55 -0700

    registed join app

--- cascadia_diving/settings.py
+++ cascadia_diving/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'users.apps.UsersConfig',
     'blog.apps.BlogConfig',
+    'join.apps.JoinConfig',
     'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
