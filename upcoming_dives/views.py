3166db12217ac05a84eecdd24e3af5b67c7469fd
Aaron Edward Glenn <53454869+AaronEdwardGlenn@users.noreply.github.com>
2020-07-02 17:48:55 -0700

    divepost form now working! Forgot to add enctype to the form

--- upcoming_dives/views.py
+++ upcoming_dives/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .forms import UpcommingDivesForm
+from django.contrib.auth.decorators import login_required
+
+
+@login_required
+def upcoming(request):
+    ud_form = UpcommingDivesForm()
+
+    context = {
+        'ud_form': ud_form
+    }
+
+    return render(request, 'upcoming_dives/upcoming_dives.html', context)
+
 
 # Create your views here.
