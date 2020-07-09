1cdaab2792cbb84814f65da34968eac6473afc4c
Aaron Edward Glenn <53454869+AaronEdwardGlenn@users.noreply.github.com>
2020-07-09 16:14:16 -0700

    added naturalist template, url route, and function based view

--- blog/views.py
+++ blog/views.py
@@ -19,27 +19,24 @@ def home(request):
     return render(request, 'blog/home.html', context)
 
 
-class PostListView(ListView):
-    model = DivePost
-    template_name = 'blog/home.html'
-    context_object_name = 'posts'
-    ordering = ['-date_posted']
-    paginate_by = 2
-
-
 class RecentDivesListView(ListView):
     model = DivePost
     template_name = 'blog/recent_dives.html'
     context_object_name = 'posts'
     ordering = ['-date_posted']
-    paginate_by = 4.
+    paginate_by = 3
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update({'recent': 'active'})
+        return context
 
 
 class UserPostListView(ListView):
     model = DivePost
     template_name = 'blog/user_diveposts.html'
     context_object_name = 'posts'
-    paginate_by = 2
+    paginate_by = 3
 
     def get_queryset(self):
         user = get_object_or_404(User, username=self.kwargs.get('username'))
@@ -57,6 +54,11 @@ class PostCreateView(LoginRequiredMixin, CreateView):
     def form_valid(self, form):
         form.instance.diver = self.request.user
         return super().form_valid(form)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update({'log': 'active'})
+        return context
 
 
 class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
@@ -111,3 +113,10 @@ def lessons(request):
         'lessons': 'active'
     }
     return render(request, 'blog/lessons.html', context)
+
+
+def naturalist(request):
+    context = {
+        'naturalist': 'active'
+    }
+    return render(request, 'blog/naturalist.html', context)
