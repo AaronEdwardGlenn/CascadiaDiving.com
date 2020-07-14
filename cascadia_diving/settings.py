d64566790ae8ba5329c63eb683272300a40a3571
Aaron Edward Glenn <53454869+AaronEdwardGlenn@users.noreply.github.com>
2020-07-14 14:21:06 -0700

    configured aws rds for db managment

--- cascadia_diving/settings.py
+++ cascadia_diving/settings.py
@@ -64,14 +64,20 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'cascadia_diving.wsgi.application'
 
+DBPASSWORD = os.environ.get('CDCEMAIL_PASSWORD')
+
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'cascadiadb',
+        'USER': 'aaronglenn',
+        'PASSWORD': DBPASSWORD,
+        'HOST': 'cascadiadb.czo9ds1rnt7y.us-west-2.rds.amazonaws.com',
+        'PORT': '5432'
     }
 }
 
